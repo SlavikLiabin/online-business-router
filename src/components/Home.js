20aa92bb353eef09a6cf29b6ca5104ce4857f310
Viacheslav Liabin <R9514633542@gmail.com>
2024-02-04 14:28:16 +0500

    rout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./HomeStyles.css";
+import Navbar from "./Navbar";
 
 const Home = () => {
   return (
     <div>
-        
+      <Navbar />
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 import "./NavbarStyles.css";
+import { FaBars, Fatimes } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <div className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Pricing
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              FAQ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className="hamburger">
+          <FaBars style={{color: '#fff'}} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
