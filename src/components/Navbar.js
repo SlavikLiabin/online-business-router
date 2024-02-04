4c1f5779d6df499047e52491b89afd53b01ff46b
Viacheslav Liabin <R9514633542@gmail.com>
2024-02-04 12:00:21 +0500

    installbase

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavbarStyles.css";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
