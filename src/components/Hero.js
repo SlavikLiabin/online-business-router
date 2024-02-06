1885b883a36d4b62f30fa855012a76635e3fe776
Viacheslav Liabin <R9514633542@gmail.com>
2024-02-06 20:59:14 +0500

    herostyles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,7 +17,6 @@ const Hero = () => {
           <div className="col-1">
             <h1>Data to enrich your</h1>
             <h1>
-              {" "}
               <span className="primary-color">online business</span>
             </h1>
             <p>
@@ -42,7 +41,41 @@ const Hero = () => {
             </div>
           </div>
           <div className="col-2">
-            <div className="form-layout"></div>
+            <div className="form-layout">
+              <div className="form-container">
+                <p className="sign-in-txt">Sign in with</p>
+                <div className="social-login">
+                  <i>
+                    <FaFacebook size={20} />
+                  </i>
+                  <i>
+                    <FaTwitter size={20} />
+                  </i>
+                  <i>
+                    <FaGithub size={20} />
+                  </i>
+                </div>
+                <div className="divider">
+                  <p>
+                    {" "}
+                    <span>Or</span>{" "}
+                  </p>
+                </div>
+                <form action="">
+                  <input type="text" placeholder="Name" />
+                  <input type="email" placeholder="Email" />
+                  <input type="password" placeholder="Password" />
+                  <button>Create your account</button>
+                </form>
+              </div>
+              <div className="form-footer">
+                <p>
+                  By signing up, you agree to our
+                  <span className="primary-color"> Terms, Data Policy</span> and
+                  <span className="primary-color"> Cookies Policy</span>.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
